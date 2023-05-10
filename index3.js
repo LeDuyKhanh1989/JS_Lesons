ac31c420586de6dd9b15834ed4e3883ea23c6043
LeDuyKhanh1989 <designer.expert228@gmail.com>
2023-05-10 19:48:13 +0700

    Bài thực hành tính diện tích hcn

---
+++ index3.js
@@ -0,0 +1,4 @@
+var dai=parseInt(prompt('Nhập vào chiều dài hình chữ nhật:'));
+var rong= parseInt(prompt('Nhập vào chiều rộng hình chữ nhật:'));
+let area= dai*rong;
+document.write('Diện tích hình chữ nhật là:'+ area);
