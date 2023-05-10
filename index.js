b5e863bfe4f2e6346b0d3f548a8c0e66618de592
LeDuyKhanh1989 <designer.expert228@gmail.com>
2023-05-10 16:35:02 +0700

    Bài thực hành variant JS

---
+++ index.js
@@ -0,0 +1,33 @@
+// 7.4 [Thực hành] Khai báo và sử dụng biến
+/*
+let i = 10
+let f = 20.5
+let b = true
+let s = 'Hà Nội'
+
+document.write('i = ' + i)
+document.write('<br/>')
+document.write('f = ' + f)
+document.write('<br/>')
+document.write('b = ' + b)
+document.write('<br/>')
+document.write('s = ' + s)
+
+// thực hành bài 2
+
+document.write('<br/>')
+document.write('<br/>')
+document.write('<br/>')
+
+let width = 20
+let height = 10
+let area = width * height
+document.write('Area = ' + area)
+*/
+
+// Thực hành bài 3 "dùng hàm prompt"
+
+var a=prompt("nhập vào giá trị a:");
+var b=prompt("Nhập vào giá trị b:");
+if(a%b==0) { alert('a là bội của b, a/b='+ (a/b))}
+else{ alert('a không phải là bội của b')}
